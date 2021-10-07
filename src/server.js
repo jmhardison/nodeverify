da84ac1ea50db6c65e1b35f41b21263976a7e8b3
Jonathan Hardison <jmh@jonathanhardison.com>
2021-10-06 22:18:29 -0500

    cleanup of old items update trivy

--- src/server.js
+++ src/server.js
@@ -7,18 +7,12 @@ var http = require('http');
 var express = require('express');
 var config = require('./config');
 var routes = require('./routes');
-var bodyParser = require('body-parser');
 
 // consts //
 let app = express();
 let configInstance = new config();
 
 app.server = http.createServer(app);
-
-//middleware
-app.use(bodyParser.json({
-    limit: configInstance.bodyLimit
-}));
 
 // Optional fallthrough error handler
 app.use(function onError(err, req, res, next) {
